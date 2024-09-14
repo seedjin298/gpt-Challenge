2bf46af78792d0597215a1ed2c805feff1e50029
seedjin298 <78636647+seedjin298@users.noreply.github.com>
2024-09-15 00:48:39 +0900

    removed spinner

--- pages/04_AssistantGPT.py
+++ pages/04_AssistantGPT.py
@@ -196,19 +196,17 @@ if is_valid:
             st.write(f"finish run: {run}")
             run_status = check_in_progress(client, run.id, thread.id)
             st.write(f"finish run_status: {run_status}")
-            # while run_status != "completed":
-            #     st.write(f"1: {run_status}")
-            #     with st.spinner("Waiting for Assistant to answer..."):
-            #         st.write(f"2: {run_status}")
-            #         if run_status == "requires_action":
-            #             st.write(f"3: {run_status}")
-            #             while run_status == "requires_action":
-            #                 st.write(f"4: {run_status}")
-            #                 submit_tool_outputs(client, run.id, thread.id)
-            #                 run_status = check_in_progress(client, run.id, thread.id)
+            while run_status != "completed":
+                st.write(f"1: {run_status}")
+                if run_status == "requires_action":
+                    st.write(f"3: {run_status}")
+                    while run_status == "requires_action":
+                        st.write(f"4: {run_status}")
+                        submit_tool_outputs(client, run.id, thread.id)
+                        run_status = check_in_progress(client, run.id, thread.id)
 
-            # if run_status == "completed":
-            #     display_answer(thread.id, question)
+            if run_status == "completed":
+                display_answer(thread.id, question)
 
 else:
     st.session_state["client"] = []
